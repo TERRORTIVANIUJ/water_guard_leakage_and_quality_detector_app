a47829b85a0fc98bb45d6a4a2bc21e3b7a17d351
Moriti-Ramolefo <ramolefo.moriti@gmail.com>
2022-10-28 04:10:30 +0200

    history page styled

--- waterquality/app/__init__.py
+++ waterquality/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 flask_app = Flask(__name__)
+CORS(flask_app)
+
 flask_app.config['SECRET_KEY'] = 'sh...-keep-this-a-secret'
 flask_app.secret_key="123"
 
